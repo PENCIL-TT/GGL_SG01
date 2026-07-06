5d326095f551092577f8655e6bf3afafe5dcc837
PENCIL-TT <hariharantt015@gmail.com>
2026-07-06 08:14:54 +0000

    vvd

--- dist/assets/label-D7Y6dh_q.js
+++
@@ -1,6 +0,0 @@
-import{c as i,r as s,j as l,P as n,v as c,aB as u}from"./index-CqtEddeU.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const h=i("FileText",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M10 9H8",key:"b1mrlr"}],["path",{d:"M16 13H8",key:"t4e002"}],["path",{d:"M16 17H8",key:"z1uh3a"}]]);var f="Label",o=s.forwardRef((e,t)=>l.jsx(n.label,{...e,ref:t,onMouseDown:a=>{var r;a.target.closest("button, input, select, textarea")||((r=e.onMouseDown)==null||r.call(e,a),!a.defaultPrevented&&a.detail>1&&a.preventDefault())}}));o.displayName=f;var d=o;const m=u("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),b=s.forwardRef(({className:e,...t},a)=>l.jsx(d,{ref:a,className:c(m(),e),...t}));b.displayName=d.displayName;export{h as F,b as L};
